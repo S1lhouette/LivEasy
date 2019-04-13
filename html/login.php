e9c8b5395d8794761204b0557a6164bd285e538d
percy <qianpercy@163.com>
2019-04-13 20:14:37 +0100

    new Login

--- html/login.php
+++ html/login.php
@@ -5,27 +5,28 @@ session_start();
 
     if ( isset ($_POST ['submit'])) {
         $error="";
-        $user = $_POST["username"];
+        $user = $_POST["email"];
         $psw = $_POST["password"];
       if(($user == "")||($psw == "")){
       }else{
         include('connect.php');
         $pdo=new PDO($dsn,$db_username,$db_password,$opt);
-        $stmt=$pdo->query("select * from usertable where userName=\"{$_POST['username']}\" and password=\"{$_POST['password']}\"");
+        $stmt=$pdo->query("select * from usertable where email=\"{$_POST['email']}\" and password=\"{$_POST['password']}\"");
         $row=$stmt->fetch(PDO::FETCH_BOTH);
         $userId=$row["userID"];
         if($userId==1){
-          $_SESSION ['user']= $_POST ['username'];
+          $_SESSION ['user']= $_POST ['email'];
           $_SESSION['userID']=$userId;
-          header();
+          header()
         }else{
           if(!empty($row[0])){//loging successfully
             //echo $username,' 欢迎你！进入 <a href="my.php">用户中心</a><br />';
-            $_SESSION ['user']= $_REQUEST ['username'];
+            $_SESSION ['user']= $user;
+            $_SESSION['userID']=$userId;
             header ("location:tenantIndex.html");
      //echo '点击此处 <a href="login.php?action=logout">注销</a> 登录！<br />';
           }else{
-            $error = " Username or Password is invalid . Try Again ";
+            $error = " Email or Password is Wrong . Try Again ";
             //echo "<Label>alert(' Username or Password is invalid . Try Again');</Label>";
             //$pdo=NULL;
           }
