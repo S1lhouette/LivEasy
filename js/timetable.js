8d5ffe43fb17c9952aa0c4e22fe22c5c16cc1a6c
tianyunHu <1034070724@qq.com>
2019-04-13 18:50:59 +0100

    Timetable update

--- js/timetable.js
+++ js/timetable.js
@@ -1,3 +1,4 @@
+var currentDay;
 function checkNull() {
     var event = document.getElementById("eventInput").value;
     var time = document.getElementById("timeList").value;
@@ -11,4 +12,38 @@ function checkNull() {
     }else if(timeInput === '' || timeInput === undefined || timeInput == null){
         alert("Time cannot be empty");
     }
+}function confirmDelete() {
+    var window = confirm("Do you want to delete this event?");
+    if (window == true){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+
+
+function changeToMyEvents() {
+    var c1 = document.getElementById("c1");
+    var c2 = document.getElementById("c2");
+    var theTitle = document.getElementById("selectedDay");
+    var btn = document.getElementById("btn");
+    btn.value = "All Events";
+    currentDay = theTitle.innerText;
+    theTitle.innerText = "My Events";
+    c1.className = "";
+    c2.className = "show";
+    btn.setAttribute("onClick","changeToAllEvents()");
+}
+
+function changeToAllEvents() {
+    var c1 = document.getElementById("c1");
+    var c2 = document.getElementById("c2");
+    var theTitle = document.getElementById("selectedDay");
+    var btn = document.getElementById("btn");
+    btn.value = "My Events"
+    theTitle.innerText = currentDay;
+    c1.className = "show";
+    c2.className = "";
+    btn.setAttribute("onClick","changeToMyEvents()");
 }
