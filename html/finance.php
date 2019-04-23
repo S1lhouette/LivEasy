d4cac9c6d68812a5b4004a3245f60aa57756b065
Derek <Z.Wang177@student.liverpool.ac.uk>
2019-04-23 16:59:57 +0100

    finance page is completed

---
+++ html/finance.php
@@ -0,0 +1,174 @@
+<?php
+error_reporting(0);
+include("connect.php");
+session_start();
+
+
+
+if(isset($_POST['confirmTransaction'])){
+  $transactionID=$_POST['transactionID'];
+  try {
+    $pdo=new PDO($dsn,$db_username,$db_password,$opt);
+    $stmt=$pdo->query("select * from transactiontable where transactionID=".$transactionID);
+    $row=$stmt->fetch();
+    $consumerList=$row['consumerID'];
+    $consumerIDArray=explode(',',$consumerList);
+    $index=0;
+    $targetIndex=-1;
+    foreach($consumerIDArray as $id){
+      if($id==$_SESSION['userID']){
+        $targetIndex=$index;
+      }
+      $index++;
+    }
+    $newConfirmState=$row['confirmState'];
+    $newConfirmState{$targetIndex}='1';
+    if(strstr($newConfirmState,"0")==false){
+      $newConfirmResult=1;
+    }else{
+      $newConfirmResult=0;
+    }
+    $stmt=$pdo->query("update transactiontable set confirmState='".$newConfirmState."', confirmResult='".$newConfirmResult."' where transactionID=".$transactionID);
+
+    $pdo=NULL;
+  } catch (PDOException $e) {
+    exit("PDO Error: ".$e->getMessage()."<br>".$transactionID);
+  }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <div id = "head">
+		<input type="submit" id="logout" value="Log out" onclick="return confirmLogout()">
+        <link  rel="stylesheet" type="text/css" href="../css/finance.css">
+		<script type="text/javascript" src="../js/finance.js"></script>
+        <a href="tenantIndex.html"><img id="logoSmall" src="../images/logo.png" alt="logo"></a>
+        <a href="tenantIndex.html" id="webName"><text id="title"> LivEasy </text></a>
+        <a href="maintenance.htm"><button class="navigation" id="myMaintenance" name="myMaintenance">My Maintenance</button></a>
+        <a href="timetable.html"><button class="navigation" id="mySchedule" name="mySchedule">My Schedule</button></a>
+        <a href = "finance.html"><button class="navigation" id="myAccounting" name="myAccounting">My Accounting</button></a>
+    </div>
+</head>
+<body id="background">
+    <div id="left">
+		<div class = "subtitles"> Accounting <br> </div>
+		<div id = "transactionDiv" style="overflow: scroll; overflow-x: hidden; border-style: none">
+			<table id = "accountingTable">
+				<thead>
+					<tr>
+						<th>Date</th>
+						<th>Payer</th>
+						<th>Cost</th>
+					</tr>
+				</thead>
+        <?php
+        try {
+          $pdo=new PDO($dsn,$db_username,$db_password,$opt);
+          $stmt=$pdo->query("select * from transactiontable inner join usertable on transactiontable.payerID=usertable.userID");
+          while($row=$stmt->fetch()){
+            $consumerList=$row['consumerID'];
+            $consumerIDArray=explode(',',$consumerList);
+            foreach($consumerIDArray as $id){
+              if($id==$_SESSION['userID']){
+                echo "<tr class='cell'>";
+                echo "<td class='date'>".$row['transactionDate']."</td>";
+                echo "<td class='payer'>".$row['name']."</td>";
+                echo "<td class='cost'>&#163;".$row['amount']."</td>";
+                echo "</td>";
+              }
+            }
+          }
+
+          $pdo=NULL;
+        } catch (PDOException $e) {
+          exit("PDO Error: ".$e->getMessage()."<br>");
+        }
+        ?>
+
+
+
+
+			</table>
+			<br>
+		</div>
+
+		<div class = "text" id = "bal">
+      <?php
+      try {
+        $pdo=new PDO($dsn,$db_username,$db_password,$opt);
+        $stmt=$pdo->query("select * from usertable where userID=".$_SESSION['userID']);
+        $row=$stmt->fetch();
+        echo "Balance: ".$row['balance']."&#163";
+        $pdo=NULL;
+      } catch (PDOException $e) {
+        exit("PDO Error: ".$e->getMessage()."<br>");
+      }
+
+      ?>
+		</div>
+
+		<div class = "text">
+			<a href = "addNewTrans.php" class = "links"><br><br>Add a new transaction</a><br>
+		</div>
+
+    </div>
+    <div id="right">
+        <div class = "subtitles"> Confirm list <br> </div>
+		<div id = "confirmDiv" style="overflow: scroll; overflow-x: hidden;">
+			<div id = "confForm">
+			<table id = "confirmTable" >
+        <?php
+        try {
+          $pdo=new PDO($dsn,$db_username,$db_password,$opt);
+          $stmt=$pdo->query("select * from transactiontable inner join usertable on transactiontable.payerID=usertable.userID");
+          while($row=$stmt->fetch()){
+            $consumerList=$row['consumerID'];
+            $consumerIDArray=explode(',',$consumerList);
+            $index=0;
+            $targetIndex=-1;
+            foreach($consumerIDArray as $id){
+              if($id==$_SESSION['userID']){
+                $targetIndex=$index;
+                $confirmStr=$row['confirmState'];
+                if(substr($confirmStr,$targetIndex,1)=="0"){
+                  echo "<form name='confirmRow' action='finance.php' method='post'>";
+                  echo "<tr class='cell'>";
+                  echo "<td class='date'>".$row['transactionDate']."</td>";
+                  echo "<td class='cost'>&#163;".$row['amount']."</td>";
+                  echo "<td class='receipt'><a href='receipt.jpg' class='links'>View receipt</a></td>";
+                  echo "<td><input type='hidden' value='".$row['transactionID']."' name='transactionID'/></td>";
+                  echo "<td class='confirm'><input type='submit' class='btn confirmBtn' name='confirmTransaction' value='Confirm'></td>";
+                  echo "</tr>";
+                  echo "</form>";
+                }
+
+              }
+              $index++;
+            }
+
+          }
+
+          $pdo=NULL;
+        } catch (PDOException $e) {
+          exit("PDO Error: ".$e->getMessage()."<br>");
+        }
+
+        ?>
+				<form name= "confirmRow" onsubmit="return confirmConfirmBtn()">
+					<tr class="cell">
+					<td class="date">2019-01-05</td>
+					<td class="cost">&#163;58</td>
+					<td class="receipt"><a href="receipt.jpg" class="links">View receipt</a></td>
+					<td class = "confirm"><input type="submit" class="btn confirmBtn" name="Confirm transaction" value="Confirm"></td>
+				</tr>
+				</form>
+			</table>
+			</div>
+			<br>
+		</div>
+    </div>
+</body>
+</html>
