e1671527406b258e9d8af7f161d030640fd2a444
Derek <Z.Wang177@student.liverpool.ac.uk>
2019-04-27 01:05:07 +0100

    充值页面改动

--- html/topup.php
+++ html/topup.php
@@ -1,3 +1,69 @@
+<?php
+error_reporting(0);
+session_start();
+include('connect.php');
+
+if(!isset($_SESSION['userID'])){
+    echo "<script type='text/javascript'>alert('Sorry, you should log in first.'); window.location.href = 'login.php';</script>";
+}
+
+if(isset($_POST['topup'])){
+  try {
+    $pdo=new PDO($dsn,$db_username,$db_password,$opt);
+
+    if($_POST['amount1']>0){
+      $stmt=$pdo->query("select * from usertable where userID=".$_POST['userID1']);
+      $row=$stmt->fetch();
+      $newBalance=$row['balance']+$_POST['amount1'];
+      $stmt2=$pdo->query("update usertable set balance=".$newBalance." where userID=".$_POST['userID1']);
+    }
+
+    if($_POST['amount2']>0){
+      $stmt=$pdo->query("select * from usertable where userID=".$_POST['userID2']);
+      $row=$stmt->fetch();
+      $newBalance=$row['balance']+$_POST['amount2'];
+      $stmt2=$pdo->query("update usertable set balance=".$newBalance." where userID=".$_POST['userID2']);
+    }
+
+    if($_POST['amount3']>0){
+      $stmt=$pdo->query("select * from usertable where userID=".$_POST['userID3']);
+      $row=$stmt->fetch();
+      $newBalance=$row['balance']+$_POST['amount3'];
+      $stmt2=$pdo->query("update usertable set balance=".$newBalance." where userID=".$_POST['userID3']);
+    }
+
+    if($_POST['amount4']>0){
+      $stmt=$pdo->query("select * from usertable where userID=".$_POST['userID4']);
+      $row=$stmt->fetch();
+      $newBalance=$row['balance']+$_POST['amount4'];
+      $stmt2=$pdo->query("update usertable set balance=".$newBalance." where userID=".$_POST['userID4']);
+    }
+
+    if($_POST['amount5']>0){
+      $stmt=$pdo->query("select * from usertable where userID=".$_POST['userID5']);
+      $row=$stmt->fetch();
+      $newBalance=$row['balance']+$_POST['amount5'];
+      $stmt2=$pdo->query("update usertable set balance=".$newBalance." where userID=".$_POST['userID5']);
+    }
+
+    if($_POST['amount6']>0){
+      $stmt=$pdo->query("select * from usertable where userID=".$_POST['userID6']);
+      $row=$stmt->fetch();
+      $newBalance=$row['balance']+$_POST['amount6'];
+      $stmt2=$pdo->query("update usertable set balance=".$newBalance." where userID=".$_POST['userID6']);
+    }
+
+
+    $pdo=NULL;
+
+
+  } catch (PDOException $e) {
+    exit("PDO Error: ".$e->getMessage()."<br>");
+  }
+}
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,38 +79,87 @@
     </div>
 </head>
 <body id="background">
-<div id="tinyTitle">Top-up to roommates</div>
-<form id="topupForm" onsubmit="return myCheck()">
+<div id="tinyTitle">Top-up for roommates</div>
+<form id="topupForm" onsubmit="return myCheck()" action="topup.php" method="post" name="topupForm">
 <table id="topupTable">
-    <tr>
-        <td><img src="../images/user.png"></td>
-        <td><div class="userName">User A</div></td>
-        <td><input type="text" class="topupInput" id="input1" onkeyup="checkNumber(this)"></td>
+  <?php
+  try {
+    $pdo=new PDO($dsn,$db_username,$db_password,$opt);
 
-        <td><img src="../images/user.png"></td>
-        <td><div class="userName">User A</div></td>
-        <td><input type="text" class="topupInput" id="input2" onkeyup="checkNumber(this)"></td>
+    $stmt=$pdo->query("select * from usertable where flatNum=".$_SESSION['flatNum']);
+    $row1=$stmt->fetch();
+    $row2=$stmt->fetch();
+    $row3=$stmt->fetch();
+    $row4=$stmt->fetch();
+    $row5=$stmt->fetch();
+    $row6=$stmt->fetch();
 
-        <td><img src="../images/user.png"></td>
-        <td><div class="userName">User A</div></td>
-        <td><input type="text" class="topupInput" id="input3" onkeyup="checkNumber(this)"></td>
-    </tr>
-    <tr>
-        <td><img src="../images/user.png"></td>
-        <td><div class="userName">User A</div></td>
-        <td><input type="text" class="topupInput" id="input4" onkeyup="checkNumber(this)"></td>
+    echo "<tr>";
 
-        <td><img src="../images/user.png"></td>
-        <td><div class="userName">User A</div></td>
-        <td><input type="text" class="topupInput" id="input5" onkeyup="checkNumber(this)"></td>
+    if(isset($row1['userID'])){
+      echo "<td><img src='../images/user.png'/></td>";
+      echo "<td><div class='userName'>".$row1['name']."<div/></td>";
+      echo "<td><input type='text' class='topupInput' id='input1' onkeyup='checkNumber(this)' name='amount1'/></td>";
+      echo "<td><input type='hidden' name='userID1' value='".$row1['userID']."'></td>";
+    }
 
-        <td><img src="../images/user.png"></td>
-        <td><div class="userName">User A</div></td>
-        <td><input type="text" class="topupInput" id="input6" onkeyup="checkNumber(this)"></td>
-    </tr>
+    if(isset($row2['userID'])){
+      echo "<td><img src='../images/user.png'/></td>";
+      echo "<td><div class='userName'>".$row2['name']."<div/></td>";
+      echo "<td><input type='text' class='topupInput' id='input2' onkeyup='checkNumber(this)' name='amount2'/></td>";
+      echo "<td><input type='hidden' name='userID2' value='".$row2['userID']."'></td>";
+    }
+
+    if(isset($row3['userID'])){
+      echo "<td><img src='../images/user.png'/></td>";
+      echo "<td><div class='userName'>".$row3['name']."<div/></td>";
+      echo "<td><input type='text' class='topupInput' id='input3' onkeyup='checkNumber(this)' name='amount3'/></td>";
+      echo "<td><input type='hidden' name='userID3' value='".$row3['userID']."'></td>";
+    }
+
+    echo "</td>";
+
+    echo "<tr>";
+
+    if(isset($row4['userID'])){
+      echo "<td><img src='../images/user.png'/></td>";
+      echo "<td><div class='userName'>".$row4['name']."<div/></td>";
+      echo "<td><input type='text' class='topupInput' id='input4' onkeyup='checkNumber(this)' name='amount4'/></td>";
+      echo "<td><input type='hidden' name='userID4' value='".$row4['userID']."'></td>";
+    }
+
+    if(isset($row5['userID'])){
+      echo "<td><img src='../images/user.png'/></td>";
+      echo "<td><div class='userName'>".$row5['name']."<div/></td>";
+      echo "<td><input type='text' class='topupInput' id='input5' onkeyup='checkNumber(this)' name='amount5'/></td>";
+      echo "<td><input type='hidden' name='userID5' value='".$row5['userID']."'></td>";
+    }
+
+    if(isset($row6['userID'])){
+      echo "<td><img src='../images/user.png'/></td>";
+      echo "<td><div class='userName'>".$row6['name']."<div/></td>";
+      echo "<td><input type='text' class='topupInput' id='input6' onkeyup='checkNumber(this)' name='amount6'/></td>";
+      echo "<td><input type='hidden' name='userID6' value='".$row6['userID']."'></td>";
+    }
+
+    echo "</td>";
+
+    $pdo=NULL;
+
+
+
+
+
+  } catch (PDOException $e) {
+    exit("PDO Error: ".$e->getMessage()."<br>");
+  }
+
+
+   ?>
+
 </table>
     <div id="hint"></div>
-<input type="submit" id="submitBtn" value="Top-up">
+<input type="submit" id="submitBtn" value="Top-up" name="topup">
 </form>
 </body>
 </html>
